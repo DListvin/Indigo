5ccd2cc3dd7fa2feff1579c2f3f1135b7a4668f5
dlistvin <dlistvin@304.ru>
2013-04-01 21:49:55 +0400

    PM: Another test in-process commit

---
+++ IndigoPrototype/IndigoEngine/Agent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IndigoEngine
+{
+    /// <summary>
+    /// Агент. Имеет местоположение и определённое состояние, которое может меняться со временем.
+    /// </summary>
+    public class Agent
+    {
+    }
+}
--- IndigoPrototype/IndigoEngine/World.cs
+++ IndigoPrototype/IndigoEngine/World.cs
@@ -10,12 +10,49 @@ namespace IndigoEngine
     /// </summary>
     interface IObservableWorld
     {
-        
+        IEnumerable<Agent> Agents {get;} //Получить список всех агентов в мире.
+        //long ModelIterations { get; } //Получить прошедшее число итераций моделирования.
+        //TimeSpan ModelIterationTick { get; set; } //Настроить промежуток времени между итерациями моделирования.
+
+        void Initialise(); //Инициализирует мир (создаёт карту).
+        void Start(); //Запускает главный цикл.
+        void Stop(); //Приостанавливает гланый цикл.
     }
+
     /// <summary>
     /// Мир - сущность, контролирующая главный цикл, включающий в себя все игровые сущности.
     /// </summary>
     public class World : IObservableWorld
     {
+        List<Agent> agents;
+        long passedModelIterations;
+        TimeSpan modelIterationTick;
+
+        public IEnumerable<Agent> Agents
+        {
+            get
+            {
+                return agents;
+            }
+        }
+
+        public void Initialise()
+        {
+            agents = new List<Agent>();
+            passedModelIterations = 0;
+            modelIterationTick = TimeSpan.FromSeconds(2);
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+
     }
 }
