5408a53aff9448d5e7c7a2993e962ea78e72df6e
dlistvin <dlistvin@304.ru>
2013-04-01 19:40:54 +0400

    PM: Создано решение, в котором будет всё. Тестовый пуш.

---
+++ IndigoPrototype/IndigoEngine/World.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IndigoEngine
+{
+    /// <summary>
+    /// Интерфейс, реализуемый миром, используемый при работе с UI.
+    /// </summary>
+    interface IObservableWorld
+    {
+        
+    }
+    /// <summary>
+    /// Мир - сущность, контролирующая главный цикл, включающий в себя все игровые сущности.
+    /// </summary>
+    public class World : IObservableWorld
+    {
+    }
+}
