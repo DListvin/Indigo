cc737b0b7cd1b5721ed8d248c29f32a2c25f8082
Nikolay Yakovlev <deylak@yandex.ru>
2013-04-02 21:30:41 +0400

    Добавлен интерфейс для типичного аггента

---
+++ IndigoPrototype/IndigoEngine/ITypicalAgent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace AgentClassLibrary
+{
+	interface ITypicalAgent
+	{
+		
+		/// <summary>
+		/// Operations with health characteristic
+		/// </summary>
+		Characteristic Health{get; set;}
+
+		/// <summary>
+		/// Operations with agent location in the world grid
+		/// </summary>
+		Point? Location{get; set;}
+
+		/// <summary>
+		/// Operations with agent inventory
+		/// </summary>
+		ItemStorage Inventory{get; set;}
+
+		/// <summary>
+		/// Making a decision about action in the current phase
+		/// </summary>
+		void Decide();	
+	}
+}
