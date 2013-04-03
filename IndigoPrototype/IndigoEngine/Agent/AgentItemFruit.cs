581b5fbe9ddcfcc0760c580bc5cb2cb2e3e84857
dlistvin <dlistvin@304.ru>
2013-04-03 09:04:09 +0400

    PM, Architecture: Запихнул исходники с агентами в отдельную папку.

---
+++ IndigoPrototype/IndigoEngine/Agent/AgentItemFruit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IndigoEngine
+{
+	class AgentItemFruit : Agent
+	{
+		#region Constructors
+			
+			public AgentItemFruit() : base()
+			{
+			}
+
+		#endregion
+
+		public override string ToString()
+		{
+			return "Item fruit: " + Name;
+		}
+
+	}
+}
---
+++ IndigoPrototype/IndigoEngine/Agent/Characteristic.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IndigoEngine
+{
+	public class Characteristic : ITypicalCharacteristic, INamabelObject
+	{
+		private string name;   //Characteristic name
+
+		private int maxValue;           //Maximum value of the characteristic
+		private const int minValue = 0; //Minimum value of the characteristic
+		private int currentValue;       //Current value of the characteristic
+
+		#region Constructors
+
+			public Characteristic()
+			{
+				Name = "Untitled characteristic";
+				
+				MaxValue = 100;
+				CurrentUnitValue = MaxValue;
+			}
+
+		#endregion
+
+		#region Properties
+
+			#region INamabelObject realisation
+				
+				public string Name
+				{
+					get
+					{
+						return name;
+					}
+					set
+					{
+						name = value;
+					}
+				}
+
+			#endregion
+
+			#region ITypicalCharacteristic realisation
+
+				public int MaxValue
+				{
+					get
+					{
+						return maxValue;
+					}
+					set
+					{	
+						if(value <= MinValue)
+						{
+							throw(new Exception(String.Format("Maximum value of {0} is less or equal to minimum: {1}!", this, value)));
+						}
+						maxValue = value;
+						if(CurrentUnitValue > MaxValue)
+						{
+							CurrentUnitValue = MaxValue;
+						}
+					}
+				}
+
+				public int MinValue
+				{
+					get
+					{
+						return minValue;
+					}
+				}
+			
+				public int CurrentUnitValue
+				{
+					get
+					{
+						return currentValue;
+					}
+					set
+					{
+						if(value < MinValue || value > MaxValue)
+						{
+							throw(new Exception(String.Format("Current value of {0} is out of borders: {1}!", this, value)));						
+						}
+						currentValue = value;
+					}
+				}
+
+				public int CurrentPercentValue
+				{
+					get
+					{
+						return (int)(((float)CurrentUnitValue / (float)MaxValue) * 100f);
+					}
+					set
+					{
+						CurrentUnitValue = (int)((float)MaxValue * (float)value / 100f);
+					}
+				}
+
+			#endregion
+
+		#endregion
+
+		public override string ToString()
+		{
+			return Name + ": " + MinValue.ToString() + "..." + CurrentUnitValue.ToString() + "..." + MaxValue.ToString();
+		}
+	}
+}
