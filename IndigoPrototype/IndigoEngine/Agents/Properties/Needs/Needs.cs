b66475a9b51dfd1864f8c3d8dd89fd321e8d2757
Zurk <kslavnov@gmail.com>
2013-04-07 23:35:13 +0400

    Needs: added NeedDrink, NeedLog

--- IndigoPrototype/IndigoEngine/Agents/Properties/Needs/Needs.cs
+++ IndigoPrototype/IndigoEngine/Agents/Properties/Needs/Needs.cs
@@ -8,19 +8,32 @@ namespace IndigoEngine
     public static class Needs
     {
 		/// <summary>
-		/// Example of need. Delete it after you invent some actual needs
+		/// Atack need
 		/// </summary>
         public static Need NeedExample
         {
-            get { return new Need("attack", 1, 0, new ActionAttack(null, null, 1)); }
+            get { return new Need("attack", 4, 1, new ActionAttack(null, null, 1)); }
         }
 
         /// <summary>
-        /// Need to eat
+        /// Eat Need
         /// </summary>
         public static Need NeedEat
         {
-            get { return new Need("exmpl", 1, 0, new ActionEat(null, null)); }
+            get { return new Need("eat", 1, 2, new ActionEat(null, null)); }
+        }
+
+        /// <summary>
+        /// Drink Need
+        /// </summary>
+        public static Need NeedDrink
+        {
+            get { return new Need("drink", 1, 1, new ActionDrink(null, null)); }
+        }
+
+        public static Need NeedLog
+        {
+            get { return new Need("log", 2, 1, new ActionObtainResourse(null, null, typeof(Agents.AgentItemLog))); }
         }
     }
 }
