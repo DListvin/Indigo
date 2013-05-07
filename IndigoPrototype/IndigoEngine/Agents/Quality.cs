3a63bc03b5c7cd07dbf373404896d66499d87b63
Zurk <kslavnov@gmail.com>
2013-05-07 12:11:55 +0400

    Agent: agent quality class added

---
+++ IndigoPrototype/IndigoEngine/Agents/Quality.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IndigoEngine.Agents
+{
+    public class Quality
+    {
+        public List<Qualities> Qualities { get; private set; }
+
+        #region Constructiors
+        public Quality()
+        {
+            Qualities = new List<Qualities>();
+        }
+
+        public Quality(List<Qualities> argQualities)
+        {
+            Qualities = argQualities;
+        }
+
+        public Quality(params Qualities[] argQualities)
+            : this()
+        {
+            foreach (Qualities qua in argQualities)
+                Qualities.Add(qua);
+        }
+        #endregion
+
+        public bool Is(Qualities qual)
+        {
+            return Qualities.Exists( val => { return val == qual;});
+        }
+    }
+
+    public enum Qualities
+    {
+        Eatable,
+        Drinkable,
+        Destroyable,
+        Desideable,
+        Moving,
+        Createable
+    }
+}
