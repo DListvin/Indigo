0c3d05ec743d5a364827e087555e99a64a64b7fa
DeyLak <deylak@yandex.ru>
2014-10-07 03:12:38 +0400

    Server: refactoring and fixed problems with sockets(actually there is need to maintain new versions of libraries)

--- IndigoDev/Server.py
+++ IndigoDev/Server.py
@@ -8,73 +8,70 @@ from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
 class PlotApplication(WebSocketApplication):
     """
     Class for WebSocket Communication
+    Includes functions overrided from base class(obvious ones)
     """
-    #def __init__(self):
-    #    WebSocketApplication.__init__(self)
-    #    self.connections = 0
 
     @staticmethod
     def ModelUpdate():
+        """
+        Function that model calls to report it's state after last tick 
+        """
         global sockets
+        
         model.simulatingWorld.map.sortAgents(model.simulatingWorld.agents)
 
         for socket in sockets:
-            socket.send(model.simulatingWorld.map.ToJson())
+            if not socket.closed:
+                socket.send(model.simulatingWorld.map.ToJson())
 
     def on_open(self):
         global connectionsCount
         global model
-        global activeQueues
+        global activePipes
         global sockets
+
         connectionsCount += 1
         sockets.append(self.ws)
 
-        print "\n" +'[on_open]: con_count - ]'+ str(connectionsCount) + "\n"
-
-        for q in activeQueues:
-            q.send(ModelState.Running)
+        for p in activePipes:
+            p.send(ModelState.Running)
+        print str(server.clients) + "\n" 
 
     def on_message(self, message):
         pass
-        #global model    
-        #model.simulatingWorld.map.sortAgents(model.simulatingWorld.agents)
-        #try:
-        #    self.ws.send(model.simulatingWorld.map.ToJson())
-        #except:
-        #    pass
 
     def on_close(self, reason):
         global connectionsCount
-        global activeQueues
-        global sokets
-        connectionsCount -= 1
-        #sockets.remove(self.ws)
+        global activePipes
+        global sockets
 
-        print "\n" +'[on_close] con_count - '+ str(connectionsCount) + "\n"
+        connectionsCount -= 1
+        sockets.remove(self.ws)
 
         if connectionsCount == 0:
-            for q in activeQueues:
-                q.send(ModelState.Paused)
+            for p in activePipes:
+                p.send(ModelState.Paused)
+
         print("Connection Closed!!!", reason)
+
 
 if __name__ == '__main__':
 
-    global connectionsCount
-    global sockets 
-    global model
-    global activeQueues
+    global connectionsCount #Current number of connected sockets
+    global sockets          #List of connected sockets
+
+    global model            #Model of this current server
+    global activePipes      #Pipe with commands to model and from model
 
     connectionsCount = 0
     sockets = []
-    activeQueues = []
-    resource = Resource({
-    '/data': PlotApplication
-})  
+    activePipes = []
+    resource = Resource({'/data': PlotApplication})  
 
     random.seed()
     newSeed = random.randrange(1000000)
-    model = Model(newSeed, activeQueues, PlotApplication.ModelUpdate)
-    model.start()
+    model = Model(newSeed, activePipes, PlotApplication.ModelUpdate)
+    model.start() #Not actually starts the model, only it's thread
 
     server = WebSocketServer(('0.0.0.0', 2345), resource, debug=True)
     server.serve_forever()
